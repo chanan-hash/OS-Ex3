d4308908c2717341e977e14590379bf7b78e1e23
chanan-hash <67066282+chanan-hash@users.noreply.github.com>
2024-08-12 20:56:03 +0300

    Adding proactor

---
+++ Q8_9/Proactor.cpp
@@ -0,0 +1,41 @@
+#include "Proactor.hpp"
+
+// get sockaddr, IPv4 or IPv6, as the multithreading
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET)
+    {
+        return &(((struct sockaddr_in *)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6 *)sa)->sin6_addr);
+}
+
+void ::Proactor::startProactor(int sockfd, proactorFunc func)
+{
+    while (true)
+    {
+        struct sockaddr_storage their_addr;
+        socklen_t addr_size;
+        int new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
+        if (new_fd == -1)
+        {
+            perror("accept");
+            continue;
+        }
+        char s[INET6_ADDRSTRLEN];
+        cout << "server: got connection from " << inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s) << endl;
+        thread t(func, new_fd, ref(mtx));
+        t.detach();
+    }
+}
+
+void ::Proactor::stopProactor()
+{
+    mtx.unlock();
+}
+
+::Proactor::~Proactor()
+{
+    stopProactor();
+}
---
+++ Q8_9/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <algorithm>
+#include <list>
+#include <string>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <poll.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sstream>
+#include <thread>
+#include <mutex>
+
+using namespace std;
+
